bd28abe31c9137eef337fc2c4760d72d89f8c0f6
Charles Buffington II <chuckinator2020@gmail.com>
2022-02-26 15:37:49 -0500

    it works somehow

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,28 +17,9 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
-import frc.robot.commands.climber.ProgressClimber;
-import frc.robot.commands.climber.StopClimber;
 import frc.robot.commands.drivetrain.RunPathPlannerTrajectory;
 import frc.robot.commands.drivetrain.SwerveTeleop;
-import frc.robot.commands.indexer.RunIndexer;
-import frc.robot.commands.indexer.StopIndexer;
-import frc.robot.commands.intake.ExtendAndIntake;
-import frc.robot.commands.intake.ExtendAndOuttake;
-import frc.robot.commands.intake.StopAndRetract;
-import frc.robot.commands.macros.IntakeAndIndex;
-import frc.robot.commands.macros.ShootAndIndex;
-import frc.robot.commands.shooter.IdleShooter;
-import frc.robot.commands.shooter.RunShooter;
-import frc.robot.commands.turret.AimTurret;
-import frc.robot.commands.turret.RunTurret;
-import frc.robot.commands.turret.StopTurret;
-import frc.robot.subsystems.ClimberSubsystem;
-import frc.robot.subsystems.IndexerSubsystem;
-import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveDrivetrain;
-import frc.robot.subsystems.TurretSubsystem;
 import frc.robot.utils.TrajectoryHelper;
 
 public class RobotContainer {
@@ -53,35 +34,13 @@ public class RobotContainer {
 
   /* Buttons */
   private final JoystickButton zeroGyro = new JoystickButton(driver, PS4Controller.Button.kCircle.value);
-  private final JoystickButton intakeAndIndex = new JoystickButton(driver, PS4Controller.Button.kL1.value);
-  private final JoystickButton extendAndOuttake = new JoystickButton(driver, PS4Controller.Button.kSquare.value);
-  private final JoystickButton shootAndIndex = new JoystickButton(driver, PS4Controller.Button.kR1.value);
-  private final JoystickButton runTurret = new JoystickButton(driver, PS4Controller.Button.kCross.value);
-  private final JoystickButton progressClimb = new JoystickButton(driver, PS4Controller.Button.kTriangle.value);
 
   /* Subsystems */
   private final SwerveDrivetrain drivetrain = new SwerveDrivetrain();
-  // private final ShooterSubsystem shooter = new ShooterSubsystem(hub);
-  // private final IntakeSubsystem intake = new IntakeSubsystem(hub);
-  // private final IndexerSubsystem indexer = new IndexerSubsystem();
-  // private final TurretSubsystem turret = new TurretSubsystem(hub);
-  // private final ClimberSubsystem climber = new ClimberSubsystem(hub);
 
   /* Commands */
   // * primitives
   private final Command c_zeroGyro = new InstantCommand( () -> drivetrain.zeroGyro() );
-  // private final Command c_extendAndOuttake = new ExtendAndOuttake(intake);
-  // private final Command c_stopAndRetract = new StopAndRetract(intake);
-  // private final Command c_idleShooter = new IdleShooter(shooter);
-  // private final Command c_stopIndexer = new StopIndexer(indexer);
-  // private final Command c_stopTurret = new StopTurret(turret);
-  // private final Command c_stopClimber = new StopClimber(climber);
-  // private final Command c_progressClimb = new ProgressClimber(climber);
-  // private final Command c_aimTurret = new AimTurret(turret);
-
-  // // * macros
-  // private final Command c_runIndexer = new IntakeAndIndex(intake, indexer);
-  // private final Command c_runShooter = new ShootAndIndex(shooter, indexer, 530.0); // 700.0 1850.0 1770.0
 
   /* Trajectories */
   private PathPlannerTrajectory tr_test_1;
@@ -107,30 +66,10 @@ public class RobotContainer {
         true, true
       )
     );
-
-    // shooter.setDefaultCommand(c_idleShooter);
-
-    // intake.setDefaultCommand(c_stopAndRetract);
-
-    // indexer.setDefaultCommand(c_stopIndexer);
-
-    // turret.setDefaultCommand(c_stopTurret);
-
-    // climber.setDefaultCommand(c_stopClimber);
   }
 
   private void configureButtonBindings () {
     zeroGyro.whenPressed(c_zeroGyro);
-    // shootAndIndex.whileHeld(c_runShooter);
-    // intakeAndIndex.whileHeld(c_runIndexer);
-    // extendAndOuttake.whileHeld(c_extendAndOuttake);
-    // runTurret.whileHeld(c_aimTurret);
-    // progressClimb.whenPressed(c_progressClimb);
-
-    // new POVButton(driver, 0).whenPressed(new RunTurret(turret, 0));
-    // new POVButton(driver, 90).whenPressed(new RunTurret(turret, 5000.0));
-    // new POVButton(driver, 270).whenPressed(new RunTurret(turret, 8000.0));
-    // new POVButton(driver, 180).whenPressed(new RunTurret(turret, -8000.0));
   }
 
   private void loadTrajectories() {
@@ -138,8 +77,8 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
+    return new WaitCommand(1.0);
     // return new RunPathPlannerTrajectory2(drivetrain, tr_test_1);
     // return null;
-    return new WaitCommand(1.0);
   }
 }
