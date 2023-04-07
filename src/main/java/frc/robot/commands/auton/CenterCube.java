f57fca7a2a02e7e55cbb0ede94a4836a525f1b20
FRCTeam1506 <frcteam1506@yahoo.com>
2023-04-07 19:44:05 -0400

    standish end saturday good code w/ current limit

---
+++ src/main/java/frc/robot/commands/auton/CenterCube.java
@@ -0,0 +1,51 @@
+package frc.robot.commands.auton;
+
+import com.pathplanner.lib.PathPlannerTrajectory;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.arm.*;
+import frc.robot.commands.drivetrain.BackwardsSlow;
+import frc.robot.commands.drivetrain.ForwardSlow2;
+import frc.robot.commands.drivetrain.RunPathPlannerTrajectory2;
+import frc.robot.commands.drivetrain.Stop;
+import frc.robot.commands.drivetrain.ZeroGyro;
+import frc.robot.commands.intake.*;
+import frc.robot.commands.macros.ground;
+import frc.robot.commands.macros.high;
+import frc.robot.commands.telescoping.SetHigh;
+import frc.robot.commands.telescoping.SetLow;
+import frc.robot.commands.telescoping.SetMid;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.OurBeautifulGlowingCANdleSubsystem;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.SwerveDrivetrain;
+import frc.robot.subsystems.TelescopingSubsystem;
+
+
+public class CenterCube extends SequentialCommandGroup {
+
+
+    //intake and outtake work for cube, so inverse for cone
+    //RA100 only for one PathPlannerTrajectory --- simple auton
+    public CenterCube (SwerveDrivetrain drivetrain, IntakeSubsystem intake, TelescopingSubsystem telescope, ArmSubsystem arm, OurBeautifulGlowingCANdleSubsystem candle, PathPlannerTrajectory trajectory1, PathPlannerTrajectory trajectory2, PathPlannerTrajectory trajectory3) {
+        
+        addCommands(
+            new DropCone(drivetrain, intake, telescope, arm, candle),
+            new armHigh(arm).withTimeout(0.3),
+            new RunPathPlannerTrajectory2(drivetrain, trajectory1, true),
+            new ParallelCommandGroup(
+                new armLow(arm),
+                new JustOuttake(intake)
+            ),
+            new ForwardSlow2(drivetrain).until(() -> drivetrain.getGyroRoll() <11),
+            new BackwardsSlow(drivetrain).withTimeout(1.25), //1.75
+            new Stop(drivetrain)
+            //new InstantCommand(() -> drivetrain.
+        );
+    }
+    
+}
